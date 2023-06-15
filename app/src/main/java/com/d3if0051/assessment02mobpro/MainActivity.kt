23c0fdae68a1a6d98e5319aced0ca63487da792f
haidarbahri <haidar123bahri@gmail.com>
2023-06-15 14:17:25 +0700

    Worker

--- app/src/main/java/com/d3if0051/assessment02mobpro/MainActivity.kt
+++ app/src/main/java/com/d3if0051/assessment02mobpro/MainActivity.kt
@@ -1,5 +1,9 @@
 package com.d3if0051.assessment02mobpro
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 import android.os.Bundle
 import android.view.View
 import android.view.ViewTreeObserver
@@ -12,10 +16,25 @@ import com.d3if0051.assessment02mobpro.viewModels.FirebaseUserViewModel
 
 class MainActivity : AppCompatActivity() {
 
+    companion object{
+        const val CHANNEL_ID = "updater"
+        const val PERMISSION_REQUEST_CODE = 1
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
         setContentView(R.layout.activity_main)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            val name = getString(R.string.channel_name)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(CHANNEL_ID, name, importance)
+            channel.description = getString(R.string.channel_desc)
+
+            val manager = getSystemService(Context.NOTIFICATION_SERVICE)
+                    as NotificationManager?
+            manager?.createNotificationChannel(channel)
+        }
     }
 }
